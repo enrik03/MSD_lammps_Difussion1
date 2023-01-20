bcbe1a3081d72a014ffe23f56f19a4164cad11ba
Enrique Casanova <enrique_patron@omar-pc>
2023-01-20 13:13:24 -0400

    arreglado el problema con la lectura del archivo xyz, le faltaban las comillas

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MSD_lammps_Difussion1
+
+# Las simulaciones de difusion son en LAMMPS y el analisis de datos en python.
+# Se calcula el coeficiente de difusion de 4 simulaciones de 1000000 particulas.
+# NVE 
--- simulaciones_generales-difussion.sh
+++ simulaciones_generales-difussion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 lammps < 90-10.in
-python3 -c'import MSD1; MSD1.MSD(10000 + 1000,100000,1000000,1000,posicion9_1.xyz)'
+python3 -c'import MSD1; MSD1.MSD(10000 + 1000,100000,1000000,1000,"posicion9_1.xyz")'
 python3 MSD2.py
 #lammps < 80-20.in
 #lammps < 70-30.in
